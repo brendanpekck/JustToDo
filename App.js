04c312c162fbe6416ef306c3a621213b2380572a
Brendan <brendanpekck@gmail.com>
2023-11-17 20:59:34 +0800

    Basic design

--- App.js
+++ App.js
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Button, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import VectorIcons from 'react-native-vector-icons/FontAwesome';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView contentContainerStyle={styles.basic}>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </ScrollView>
   );
 }
 
+function CalendarScreen() {
+  return (
+    <ScrollView contentContainerStyle={styles.basic}>
+      <View>
+        <Text>Calendar</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <ScrollView contentContainerStyle={styles.basic}>
+      <View>
+        <Text>Account</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+function SettingScreen() {
+  return (
+    <ScrollView contentContainerStyle={styles.basic}>
+      <View>
+        <Text>Settings</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({focused}) => (<VectorIcons name="list" color={focused? "#ff6347": "#808080"} size={25}/>)
+            }}
+          />
+          <Tab.Screen
+            name="Calendar"
+            component={CalendarScreen}
+            options={{
+              tabBarLabel: "Calendar",
+              tabBarIcon: ({focused}) => (<VectorIcons name="calendar" color={focused? "#ff6347": "#808080"} size={25}/>)
+            }}
+          />
+          <Tab.Screen
+            name="Account"
+            component={AccountScreen}
+            options={{
+              tabBarLabel: "Account",
+              tabBarIcon: ({focused}) => (<VectorIcons name="user-circle-o" color={focused? "#ff6347": "#808080"} size={28}/>)
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingScreen}
+            options={{
+              tabBarLabel: "Settings",
+              tabBarIcon: ({focused}) => (<VectorIcons name="gear" color={focused? "#ff6347": "#808080"} size={30}/>)
+            }}
+          />
+        </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
+  basic: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    borderWidth: 1,
+    marginTop: StatusBar.currentHeight
+  }
 });
